bd3e2f827d95e426e0d0a5b04b416485636c3c67
med <mohamedelhanine94@gmail.com>
2018-08-31 17:29:06 +0100

    fixed error

--- login/index.php
+++ login/index.php
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
             <div class="full-page login-page"  data-color="blue">
                 <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
                 <div class="content">
-                    <div class="container">
+                    <div class="container" style="max-width : 1170px !important">
 
                         <div class="row">
                             <div class="col-md-4 col-sm-6 col-md-offset-4 col-sm-offset-3">
